f523aa8dc42945a7cfb9dff6cb225a04d228cfe1
Apro <aprofromuk-work@yahoo.co.in>
2015-08-05 11:40:45 +0200

    Initial Commit

---
+++ Share SoundCloud/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Share SoundCloud
+//
+//  Created by Apro on 05/08/15.
+//  Copyright (c) 2015 Apro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
